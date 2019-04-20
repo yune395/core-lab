25645912218e36fb0af6362278330a6527936caf
Emily Yun <yune395@newschool.edu>
2019-04-20 14:26:27 -0400

    6

--- game/pickup.js
+++ game/pickup.js
@@ -26,7 +26,8 @@ $('.profile1').click(function(e){
         $('#div').fadeIn('slow');
 
 });
-$('#dislikebutton').click(function(e){
+
+/*$('#dislikebutton').click(function(e){
         $('.alex').hide('none');
         $('.alexbio').hide('none');
         $('#container').hide('none');
@@ -38,8 +39,27 @@ $('#dislikebutton').click(function(e){
   $('#container2').hide('none');
   $('#container3').fadeIn('slow');
 
+});*/
+
+$('#like').click(function(e){
+  $('#option1,#option1text, #option2, #option2text, #option3, #option3text').fadeIn('slow');
+});
+$('#option1').click(function(e){
+  $('#div').fadeOut('slow');
+    $('#div').fadeIn('slow');
 });
 
+$('#keepswiping').click(function(){
+   window.location.href='gamepage2practice.html';
+});
+$('#option2').click(function(){
+   window.location.href='match.html';
+});
+
+$('#option1').click(function(e){
+  $('#div').fadeOut('slow');
+    $('#div').fadeIn('slow');
+});
 /*var dislike = document.getElementById("#dislikebutton");
 var container = document.getElementById("#container");
 var container1 = document.getElementById("#container1");
@@ -70,12 +90,12 @@ dislike.onclick = displayContainer;*/
     $('#container' + currentImage).stop().fadeIn(1);
   });*/
 
-  var items = [container, container2, container3, 4, 5, 6, 7, 8, 9];
+  /*var items = [container, container2, container3, 4, 5, 6, 7, 8, 9];
   $('#dislikebutton').click(function() {
     var rand = items[Math.floor(Math.random() * items.length)];
     $('#display').text(rand);
     items.splice(items.indexOf(rand), 1);
-  })
+  })*/
 
 //window.onload = function(){
   //  window.setTimeout('document.getElementById("mail").style.display = "inline";',2000);
@@ -91,3 +111,22 @@ dislike.onclick = displayContainer;*/
   //  $("#option1answer").css("display","block")
 
 //});
+var aNumber = ++
+var tinderBios = [
+	{
+	"name":"Ari",
+	"age": 21,
+	"distance": "3 miles away",
+	"bio": "if you're not just as obsessed with Game of Thrones as I am, we can't be friends.",
+	},
+	{
+	"name":"Bobbie",
+	"age": 25,
+	"distance": "15 miles away",
+	"bio": "Some cute flirty bio",
+	},
+]
+
+$("#dislikebutton").click(function() {
+    $('.alex').text(tinderBios[aNumber]['name']);
+ });
