5eeab64f2205c811ec0e6f5679b02441579fb8af
Emily Yun <yune395@newschool.edu>
2019-04-24 14:33:05 -0400

    8

--- game/pickup.js
+++ game/pickup.js
@@ -29,7 +29,7 @@ $('.profile1').click(function(e){
 $("#begin").click(function(){
   $("div").fadeOut();
   $('#description').fadeIn('slow');
-  $('#intropageclick').fadeIn('slow');
+  $('#heartclick').fadeIn('slow');
 });
 
 
@@ -54,18 +54,18 @@ $('#dislikebutton').click(function(e){
 
 
 $('#keepswiping').click(function(){
-   window.location.href='gamepage2practice.html';
+   window.location.href='gamepage1practice.html';
 });
 
-$('#answer2').click(function(){
-   window.location.href='nomatch.html';
-   $('#answer2').delay(5000);
-});
-
-$('#kaden2').click(function(){
-   window.location.href='nomatch.html';
-   $('#answer2').delay(5000);
-});
+// $('#answer2').click(function(){
+//    window.location.href='nomatch.html';
+//    $('#answer2').delay(5000);
+// });
+//
+// $('#kaden2').click(function(){
+//    window.location.href='nomatch.html';
+//    $('#answer2').delay(5000);
+// });
 
 
 /*$('#answer1text').click(function(){
@@ -100,21 +100,19 @@ $('#option3').click(function(){
 //    window.location.href='bobbie.html';
 // });
 
+
 //alex match alert
-
 $("#answer1text").click(function(){
+  $("div").fadeOut("slow");
+  $("#like").fadeOut("slow");
+  $("#dislikebutton").fadeOut("slow");
   $("#notification").fadeIn("slow");
-});
-$("#notification").click(function(){
-$("#notification").fadeOut("slow");
- window.location.href='gamepage1practice.html';
+  $("#match").show("slow");
+    $("#keepswiping").show("slow");
+    $("#keepswipingtext").show("slow");
 });
 
-//kaden
-$("#kaden3text").click(function(){
-  $("#notificationkaden").fadeIn("slow");
-  $(".dismiss").fadeIn("slow");
-});
+
 
 var bios = [
 {
@@ -137,185 +135,185 @@ var bios = [
   "name":"Ari,",
   "age": 24,
   "bio": "If you’re not just as obsessed with Game of Thrones as I am, we can’t be friends..",
-  "correctAnswer": 87,
-  "answer2": 11,
-  "answer3": 0,
+  "correctAnswer": "did you get sacrificed by the God of Fire ? Because you’re smoking!",
+  "answer2": "I may not be the one who lived, but I can still be your chosen one.",
+  "answer3": "Are you a Snitch? Because you’re by far the greatest catch here.",
 },
 {
   "name":"Charlie,",
   "age": 20,
   "bio": "I LOVE cats. I’m okay with being 4ever alone with 50 cats, but maybe we can raise them 2gether?",
-  "correctAnswer": 9,
-  "answer2": 4,
-  "answer3": 7,
+  "correctAnswer": "MEOW you doin?",
+  "answer2": "Yeah, I like to chase my tail, but I’d much rather chase yours.",
+  "answer3": "If you were vegetable, you’d be a cute-cumber.",
 },
 {
   "name":"Daryl,",
   "age": 28,
   "bio": "#420friendly #blazeitdon’tpraiseit #snoopdog dude, lets smoke.",
-  "correctAnswer": "hello",
-  "answer2": 83,
-  "answer3": 7,
+  "correctAnswer": 45,
+  "answer2": 34,
+  "answer3": 2,
 },
 {
   "name":"Harley,",
   "age": 28,
   "bio": "Trying to find someone to come with me to a rave and be my EDM QUEEN.",
-  "correctAnswer": "hello",
-  "answer2": 83,
+  "correctAnswer": 78,
+  "answer2": 4,
   "answer3": 7,
 },
 {
   "name":"Jesse,",
   "age": 28,
-  "bio": "i love kpop! BTS all day everyday. let’s listen together?",
-  "correctAnswer": "hello",
-  "answer2": 83,
-  "answer3": 7,
+  "bio": "i love jazz!",
+  "correctAnswer": 35,
+  "answer2": 5,
+  "answer3": 9,
 },
 {
   "name":"Kyle,",
   "age": 28,
   "bio": "I love Minecraft and gaming:)",
-  "correctAnswer": "hello",
-  "answer2": 83,
-  "answer3": 7,
+  "correctAnswer": 2,
+  "answer2": 9,
+  "answer3": 4,
 },
 {
   "name":"Lou,",
   "age": 28,
-  "bio": "art hoe;) let’s spend our days together painting all day during sunset.",
-  "correctAnswer": 86,
-  "answer2": 83,
-  "answer3": 7,
+  "bio": "artsy! let’s spend our days together painting all day during sunset.",
+  "correctAnswer":09,
+  "answer2": 3,
+  "answer3": 6,
 },
 {
   "name":"Reed,",
   "age": 28,
   "bio": "I love being in nature. Let’s live in the forrest and save Mother Earth. ",
-  "correctAnswer": "hello",
-  "answer2": 83,
-  "answer3": 7,
+  "correctAnswer": 24,
+  "answer2": 45,
+  "answer3": 3,
 },
 {
   "name":"Sam,",
   "age": 28,
   "bio": "Aspiring doctor. NYU. I might be able to save your life one day;)",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 25,
+  "answer2": 87,
+  "answer3": 5,
 },
 {
   "name":"Val,",
   "age": 28,
   "bio": "gainz all day. looking for my gym partner so we can stunt on everyone with our muscles.",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 45,
+  "answer2": 2,
+  "answer3": 6,
 },
 {
   "name":"Winter,",
   "age": 28,
   "bio": "dancing is my life. ",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 36,
+  "answer2": 37,
+  "answer3": 6,
 },
 {
   "name":"Stevie,",
   "age": 28,
   "bio": "photo major. Looking for models and friends. let’s link and shoot!",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 9,
+  "answer2": 7,
+  "answer3": 3,
 },
 {
   "name":"Spencer,",
   "age": 28,
   "bio": "Software engineer working for a design agency. Let me fix your computer lol.",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 6,
+  "answer2": 4,
+  "answer3": 2,
 },
 {
   "name":"Sean,",
   "age": 21,
   "bio": "animal lover!! My dog, Jack, is my best friend 4life. If you have a furry friend, let’s hang!",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 5,
+  "answer2": 6,
+  "answer3": 3,
 },
 {
   "name":"Peyton,",
   "age": 21,
   "bio": "Filmmaker in NYC. Who’s your favorite director?",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 4,
+  "answer2": 2,
+  "answer3": 8,
 },
 {
   "name":"Graham,",
   "age": 28,
   "bio": "Fitness Maniac! If I’m not working, you can catch me at the gym. Let’s bulk.",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 7,
+  "answer2": 5,
+  "answer3": 4,
 },
 {
   "name":"Jan,",
   "age": 24,
   "bio": "Illustrator. Message me and I’ll possibly draw a portrait of you!",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 3,
+  "answer2": 7,
+  "answer3": 9,
 },
 {
   "name":"Mitchell,",
   "age": 25,
   "bio": "Interested in science and biology! currently studying at NYU Stern.",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 0,
+  "answer2": 2,
+  "answer3": 6,
 },
 {
   "name":"Ray,",
   "age": 23,
   "bio": "Hopeless romantic. Looking for a serious relationship. ",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 4,
+  "answer2": 3,
+  "answer3": 6,
 },
 {
   "name":"Max,",
   "age": 20,
   "bio": "creating my own fashion line. only name brands or gtfo",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 7,
+  "answer2": 09,
+  "answer3": 34,
 },
 {
   "name":"Monroe,",
   "age": 21,
   "bio": "i love Harry Potter. I’m definitely Griffindor, wbu? :)",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 64,
+  "answer2": 3,
+  "answer3": 7,
 },
 {
   "name":"Ken,",
   "age": 23,
   "bio": "Vegan. THE MEAT INDUSTRY KILLS.",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "correctAnswer": 3,
+  "answer2": 4,
+  "answer3": 9,
 },
 {
   "name":"Louise,",
   "age": 21,
-  "bio": "I’m a scorpio;) I’m crazy and spontaneous. Looking for a Cancer to handle me. ",
-  "correctAnswer": "hello",
-  "answer2": "hello",
-  "answer3": "hello",
+  "bio": "I’m a scorpio. I’m crazy and spontaneous.",
+  "correctAnswer": 2,
+  "answer2": 0,
+  "answer3": 54,
 },
 
 
@@ -342,16 +340,48 @@ $("#dislikebutton").click(function(){
 })
 
 $("#like").click(function(){
-var divAppend = ["<div id='answer1'><div id='answer1text'></div></div>", "<div id='answer2'><div id='answer2text'></div></div>" , "<div id='answer3'><div id='answer3text'></div></div>"];
+var divAppend = ["<div class='answerclass' id='answer1'><div class='answertext' id='answer1text'></div></div>",
+                  "<div class='answerclass' id='answer2'><div class='answertext' id='answer2text'></div></div>" ,
+                  "<div class='answerclass' id='answer3'><div class='answertext' id='answer3text'></div></div>"];
 var divNum = Math.floor(Math.random() * divAppend.length);
 
+// append pick up lines (that correspond to a specific person)
+// get specific person
+// get pick up lines
+// append:
 $("#answers").append(divAppend[0]);
 $("#answers").append(divAppend[2]);
 $("#answers").append(divAppend[1]);
 
-  $("#answer1text").text(bios[randomNumber]["correctAnswer"]);
-  $("#answer2text").text(bios[randomNumber]["answer2"])
-  $("#answer3text").text(bios[randomNumber]["answer3"])
+// one of those pick up lines needs a correct tag
+var answers = $("#answers").find(".answerclass"),
+  amountOfAnswers = answers.length;
+  var ourcorrectAnswer = Math.floor(Math.random() * answers.length);
+  console.log(ourcorrectAnswer) // give us an index for the correct answer
+
+  $(".answerclass").eq(ourcorrectAnswer).addClass("correctAnswer").find(".answertext").text(bios[randomNumber]["correctAnswer"])
+  // $("#answer1text").text(bios[randomNumber]["correctAnswer"]);
+
+  var firstanswer = Math.ceil(Math.random()*2) // choose the order of the other answers
+
+  $(".answerclass").each(function(){
+    if(!$(this).hasClass("correctAnswer")){
+      console.log(firstanswer)
+      if(firstanswer == 1){
+          $(this).find(".answertext").text(bios[randomNumber]["answer3"])
+          firstanswer = 2;// update firstanswer so that second answer is assigned to the other answer
+      }else{
+        $(this).find(".answertext").text(bios[randomNumber]["answer2"])
+        firstanswer = 1;// update firstanswer so that second answer is assigned to the other answer
+      }
+
+    }
+  })
+
+  // maybe add the on click functions here
+
+  // $("#answer2text").text(bios[randomNumber]["answer2"])
+  //$("#answer3text").text(bios[randomNumber]["answer3"])
   $("#answers").show();
 })
 
