61d2a27a878f2d4f6ddbf795a235ec5cf57a0dec
Emily Yun <yune395@newschool.edu>
2019-05-01 15:12:46 -0400

    7

--- game/pickup.js
+++ game/pickup.js
@@ -1,9 +1,11 @@
+$( document ).ready(function() {
+
 var correctGuesses = 0;
 
 /////////INTRO PAGE FUNCTIONS//////////
 
 $('#intropageclick').click(function(e){
-    $("div").fadeOut();
+        $("div").fadeOut();
         $('#description').fadeIn('slow');
         $('#heartclick').fadeIn('slow');
 });
@@ -13,14 +15,12 @@ $('#heartclick').click(function(e){
 });
 
 $("#begin").click(function(){
-  $("div").fadeOut();
-  $('#description').fadeIn('slow');
-  $('#heartclick').fadeIn('slow');
+        $("div").fadeOut();
+        $('#description').fadeIn('slow');
+        $('#heartclick').fadeIn('slow');
 });
 
 ////////////////////////////////////////////
-
-
 
 
 $("#answers").hide();
@@ -28,26 +28,32 @@ $("#answers").hide();
 var randomNumber = Math.floor(Math.random() * bios.length);
 // console.log(randomNumber)
 
+
 $(".user").text(bios[randomNumber]["name"])
 $(".userbio").text(bios[randomNumber]["bio"])
 $(".age").text(bios[randomNumber]["age"])
 
-$("#dislikebutton").click(function(){
-  var randomNumber = Math.floor(Math.random() * bios.length);
-  // console.log(randomNumber)
 
-  $(".user").text(bios[randomNumber]["name"])
-  $(".userbio").text(bios[randomNumber]["bio"])
-  $(".age").text(bios[randomNumber]["age"])
+$("#dislikebutton").click(function(){
+        randomNumber = Math.floor(Math.random() * bios.length);
+    // console.log(randomNumber)
+        $(".user").text(bios[randomNumber]["name"])
+        $(".userbio").text(bios[randomNumber]["bio"])
+        $(".age").text(bios[randomNumber]["age"])
 })
 
-$("#like").one('click',function(){
+/////////click on heart and answers append at bottom of screen///////////
+
+$("#like").on('click',function(){
+
+$("#answers").html("")
+
+console.log("test: ", $("#answers").html())
 
 var divAppend = ["<div class='answerclass' id='answer1'><div class='answertext' id='answer1text'></div></div>",
                   "<div class='answerclass' id='answer2'><div class='answertext' id='answer2text'></div></div>" ,
                   "<div class='answerclass' id='answer3'><div class='answertext' id='answer3text'></div></div>"];
 var divNum = Math.floor(Math.random() * divAppend.length);
-
 
 // append pick up lines (that correspond to a specific person)
 // get specific person
@@ -56,56 +62,65 @@ var divNum = Math.floor(Math.random() * divAppend.length);
 $("#answers").append(divAppend[0]);
 $("#answers").append(divAppend[2]);
 $("#answers").append(divAppend[1]);
-});
 
-$("#like").click(function(){
+console.log("round 2: ", $("#answers").html())
+
 // one of those pick up lines needs a correct tag
-var answers = $("#answers").find(".answerclass"),
-  amountOfAnswers = answers.length;
-  var ourcorrectAnswer = Math.floor(Math.random() * answers.length);
+      var answers = $("#answers").find(".answerclass"),
+      amountOfAnswers = answers.length;
+      var ourcorrectAnswer = Math.floor(Math.random() * answers.length);
   // console.log(ourcorrectAnswer) // give us an index for the correct answer
+      // clear on each like to prepare for new filling
+        $(".answerclass").eq(ourcorrectAnswer).addClass("correctAnswer")
+        $(".answerclass").eq(ourcorrectAnswer).find(".answertext").text(bios[randomNumber]["correctAnswer"])
 
-  $(".answerclass").eq(ourcorrectAnswer).addClass("correctAnswer").find(".answertext").text(bios[randomNumber]["correctAnswer"])
 
   // $("#answer1text").text(bios[randomNumber]["correctAnswer"]);
 
-  var firstanswer = Math.ceil(Math.random()*2) // choose the order of the other answers
+    var firstanswer = Math.ceil(Math.random()*2) // choose the order of the other answers
 
-  $(".answerclass").each(function(){
+$(".answerclass").each(function(){
     if(!$(this).hasClass("correctAnswer")){
       // console.log(firstanswer)
-      if(firstanswer == 1){
-          $(this).find(".answertext").text(bios[randomNumber]["answer3"])
+      // already has content
+    if(firstanswer == 1){
+        $(this).find(".answertext").text(bios[randomNumber]["answer3"])
           firstanswer = 2;// update firstanswer so that second answer is assigned to the other answer
-      } else{
+    }else{
         $(this).find(".answertext").text(bios[randomNumber]["answer2"])
         firstanswer = 1;// update firstanswer so that second answer is assigned to the other answer
       }
-    } else{
-      $(this).removeClass("answerclass");
-    }
-  })
+    }else{
+        $(this).removeClass("answerclass");
+      }
+})
 
-//click the right answer, heart animation/text fades in
-  $(".correctAnswer").click(function(){
-    $('#rose').fadeIn('slow');
-    $('.wrong').hide();
-    correctGuesses++;
-    console.log("correct:", correctGuesses);
-   $('#notification').fadeIn('slow');
-   $('#match').fadeIn('slow');
-   //$('#keepswiping').fadeIn('slow');
-    $('#keepswipingtext').fadeIn('slow');
-    $( ".keepswipingtext" ).removeClass();
-    $('#notification').removeClass('blurfilter');
-    $('div').addClass('blurfilter');
-    $('#like').addClass('blurfilter');
-    $('#dislikebutton').addClass('blurfilter');
-      //Add that person to your match list
-      $(".matches").html(bios[randomNumber]["name"])
-   });
+//////////CORRECT ANSWER CLICK FUNCTION///////////
 
-//click the wrong answer, broken heart and text fades in
+$(".correctAnswer").click(function(){
+        $(".correctAnswer").css({"color" : "#4be295"});
+        $(".correctAnswer").css({"border" : "2px solid #4be295"});
+        $('#rose').fadeIn('slow');
+        $('.wrong').fadeOut();
+        correctGuesses++;
+        console.log("correct:", correctGuesses);
+        $('#notification').fadeIn('slow');
+        $('#match').fadeIn('slow');
+       //$('#keepswiping').fadeIn('slow');
+        $('#keepswipingtext').fadeIn('slow');
+        $(".keepswipingtext" ).removeClass();
+        $('#notification').removeClass('blurfilter');
+        $('div').addClass('blurfilter').delay(1000);
+        $('#like').addClass('blurfilter');
+        $('#dislikebutton').addClass('blurfilter');
+          ///Adding that person to your match list
+        $(".matches").html(bios[randomNumber]["name"])
+        // here we might want to get rid of our answers to we can reload them nextime
+        $("#answers").html("")
+});
+
+//////////WRONG ANSWER CLICK FUNCTION///////////
+
 $(".answerclass").click(function(){
         $('.wrong').show();
         $('div').addClass('blurfilter');
@@ -115,32 +130,32 @@ $(".answerclass").click(function(){
         $('.wrong').removeClass('blurfilter');
         $('#keepswipingtext').fadeIn('slow');
         $( ".keepswipingtext" ).removeClass();
-        });
+});
 
-//remove blur filter on heart animation and text
-
-
+//////////KEEP SWIPING CLICK FUNCTION///////////
+//continue game after right/wrong answer click
 
 $('#keepswipingtext').click(function(){
-  // Instead of reload
-  $("#match" ).hide();
-  $("#nomatch" ).hide();
-  $("#brokenheart").hide();
-  $( "#rose" ).hide();
-   $("#answers").hide();
-   $("#answers").css("top","0vh");
-   $("#like").css("opacity","1");
-   $('#notification').hide();
-   $('#keepswipingtext').hide();
-  $('div').removeClass('blurfilter');
- $('#like').removeClass('blurfilter');
-  $("#dislikebutton").show();
- $('#dislikebutton').removeClass('blurfilter');
+        $("#match" ).hide();
+        $("#nomatch" ).hide();
+        $("#brokenheart").hide();
+        $( "#rose" ).hide();
+        $("#answers").hide();
+        $("#answers").css("top","0vh");
+        $("#like").css("opacity","1");
+        $('#notification').hide();
+        $('#keepswipingtext').hide();
+        $('div').removeClass('blurfilter');
+        $('#like').removeClass('blurfilter');
+        $("#dislikebutton").show();
+        $('#dislikebutton').removeClass('blurfilter');
 
- var randomNumber = Math.floor(Math.random() * bios.length);
- $(".user").text(bios[randomNumber]["name"])
- $(".userbio").text(bios[randomNumber]["bio"])
- $(".age").text(bios[randomNumber]["age"])
+//RANDOMIZE BIOS AFTER KEEP SWIPING TEXT IS CLICKED
+
+ randomNumber = Math.floor(Math.random() * bios.length);
+        $(".user").text(bios[randomNumber]["name"])
+        $(".userbio").text(bios[randomNumber]["bio"])
+        $(".age").text(bios[randomNumber]["age"])
   // Repeat the answer process of randomizing and changing the text
   // var answers = $("#answers").find(".answerclass"),
   //   amountOfAnswers = answers.length;
@@ -165,52 +180,74 @@ $('#keepswipingtext').click(function(){
   //
   //     }
   //   })
-  // window.location.href='gamepage1practice.html';
 });
-
-
-  // maybe add the on click functions here
-
-  // $("#answer2text").text(bios[randomNumber]["answer2"])
-  //$("#answer3text").text(bios[randomNumber]["answer3"])
-  $("#answers").show();
-  $("#answers").css("top","-20vh");
-  $("#like").css("opacity","0");
+//below are still part of the LIKE click function
+        $("#answers").show();
+        $("#like").css("opacity","0");
 })
+
+///////LIKE button smooth scroll function/////////
 
 $("#like").click(function() {
-    $('html,body').animate({
+        $('html,body').animate({
         scrollTop: $("#answers").offset().top},
         'slow');
-  $( "#dislikebutton" ).hide();
-
+        $( "#dislikebutton" ).hide();
 });
 
-$("#matchestitle").click(function(){
-  $("#containermatch").css({"transform": "translate(-90vw, 0vh)"})
-  $("#containermatch2").show();
-  $("#containermatch2").css({"transform": "translate(0vw, -50vh)"})
+// TOTAL MATCHES PROTOTYPE
+// $("#matchestitle").click(function(){
+//   $("#containermatch").css({"transform": "translate(-90vw, 0vh)"})
+//   $("#containermatch2").show();
+//   $("#containermatch2").css({"transform": "translate(0vw, -50vh)"})
+// })
 
-})
+////////// 60s COUNTDOWN TIMER FOR END GAME /////////
+// var myTimer = function(){
+//   setTimeout(function(){
+//           $("div").hide();
+//           $("#like").hide();
+//           $(".tryagain").show();
+//           $("#endheart").show();
+//           $("div").removeClass('blurfilter');
+//           $("#keepswipingtext").hide();
+//           $("#dislikebutton").hide();
+//           $(".score").show();
+//           $("#matches").show();
+//           $(".score").text(correctGuesses + " matches");
+//           $('html,body').animate({
+//               scrollTop: $("html").offset().top},
+//               'slow');
+//           $('body').animate({"background-color" : "red"});
+//   }, 10000);
+// }
 
-$("#keepswipingtext").click(function(){
-  $("#keepswipingtext").css({"transform": "translate(-30vw, -70vh)"})
-  $("#containermatch2").css("opacity","0");
-})
 
-//////////Countdown do these things when time runs out/////////
-// setTimeout(function(){
-//   $("div").hide();
-//   $("#like").hide();
-//   $("#notification").show();
-//   $("div").removeClass('blurfilter');
-//   $("#keepswipingtext").hide();
-//   $("#dislikebutton").hide();
-//   $(".score").show();
-//   $("#matches").show();
-//   $(".score").text(correctGuesses + " matches");
-//   $('html,body').animate({
-//       scrollTop: $("html").offset().top},
-//       'slow');
-//   $('body').animate({"background-color" : "red"});
-// }, 60000);
+///on click try again button change to green and RANDOMIZE BIOS AGAIN///
+
+$(".tryagain").click(function() {
+        $(".tryagain").css({"color" : "#4be295"});
+        $(".tryagain").css({"border" : "2px solid #4be295"});
+        $( "#matches").delay(1000).fadeOut(0);
+        $( ".score").delay(1000).fadeOut(0);
+        $( ".tryagain").delay(1000).fadeOut(0);
+        $( "#endheart").delay(1000).fadeOut(0);
+        $("#answers").show();
+        $("#container").delay(1000).fadeIn();
+        $("#like").delay(1000).fadeIn(0);
+        $("#dislikebutton").delay(1000).fadeIn();
+
+        randomNumber = Math.floor(Math.random() * bios.length);
+               $(".user").text(bios[randomNumber]["name"])
+               $(".userbio").text(bios[randomNumber]["bio"])
+               $(".age").text(bios[randomNumber]["age"])
+       //below are still part of the LIKE click function
+       myTimer()
+});
+
+
+
+
+
+
+});
